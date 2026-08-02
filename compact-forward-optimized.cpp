00b0d331da9721042d7106cd30c20dd711760ca3
GokulnaathB <bgokulnaath@gmail.com>
2026-08-02 22:08:08 +0530

    optimize the compact forward approach to remove the sorting and degrees altogether, and instead make use of vertex ids

---
+++ compact-forward-optimized.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // #############################################
+    // Taking graph edges input from the user.
+    vector<pair<int, int>> edges;
+    cout << "ENTER THE EDGES OF THE GRAPH\n\n";
+    int x, y, z;
+    int count = 1;
+    while (true)
+    {
+        cout << "Enter the starting vertex of edge " << count << ": ";
+        cin >> x;
+        cout << "Enter the ending vertex of edge " << count << ": ";
+        cin >> y;
+        cout << "\n";
+        edges.push_back({x, y});
+        cout << "Enter 1 to continue or 0 to quit: ";
+        cin >> z;
+        if (z == 1)
+        {
+            cout << "\n";
+            count += 1;
+            continue;
+        }
+        if (z == 0)
+        {
+            cout << "\n";
+            break;
+        }
+        while (z)
+        {
+            cout << "Enter only either 1 or 0, 1 to continue and 0 to quit: ";
+            cin >> z;
+            if (z == 1)
+            {
+                cout << "\n";
+                count += 1;
+                break;
+            }
+            if (z == 0)
+            {
+                cout << "\n";
+                break;
+            }
+        }
+        if (!z)
+            break;
+    }
+
+    // #############################################
+    // Finding the highest node value.
+    x = INT_MIN;
+    for (auto edge : edges)
+    {
+        x = max(x, edge.first);
+        x = max(x, edge.second);
+    }
+
+    // #############################################
+    // Finding how many neighbors each node has got.
+    vector<int> neighbor_count(x + 1, 0);
+    for (auto edge : edges)
+    {
+        neighbor_count[edge.first] += 1;
+        neighbor_count[edge.second] += 1;
+    }
+
+    // #############################################
+    // Finding what the size of values array should be.
+    int s = 0;
+    for (auto count : neighbor_count)
+        s += count;
+
+    // #############################################
+    // Declaring and initializing data structures to keep track of the next index where a neighbor of a node must be placed in values.
+    vector<int> node_idx(x + 1, 0);
+    int prev_ending = neighbor_count[0] - 1;
+    node_idx[0] = 0;
+    for (int i = 1; i <= x; i += 1)
+    {
+        node_idx[i] = prev_ending + 1;
+        prev_ending = prev_ending + neighbor_count[i];
+    }
+
+    vector<int> ptrs(x + 2, 0);
+    for (int i = 0; i <= x; i += 1)
+        ptrs[i] = node_idx[i];
+    ptrs[x + 1] = s;
+
+    // #############################################
+    // CSR-Representing the graph.
+    vector<int> values(s, 0);
+    int u, v, idx_u, idx_v;
+    for (auto edge : edges)
+    {
+        u = edge.first, v = edge.second;
+        idx_u = node_idx[u], idx_v = node_idx[v];
+        values[idx_u] = v, values[idx_v] = u;
+        node_idx[u] += 1, node_idx[v] += 1;
+    }
+
+    // #############################################
+    // Constructing the flat forward neighbors array.
+    vector<int> fwd_neighbors;
+    vector<int> f_ptrs(x + 2, 0);
+    int l, r;
+    for (int i = 0; i <= x; i += 1)
+    {
+        f_ptrs[i] = fwd_neighbors.size();
+        l = ptrs[i], r = ptrs[i + 1] - 1;
+        for (int j = l; j <= r; j += 1)
+            if (values[j] > i)
+                fwd_neighbors.push_back(values[j]);
+    }
+    f_ptrs[x + 1] = fwd_neighbors.size();
+
+    // #############################################
+    // Counting the triangles.
+    count = 0;
+    vector<int> visited(x + 1, 0);
+    int w;
+    int L, R;
+    for (int i = 0; i <= x; i += 1)
+    {
+        u = i;
+        l = f_ptrs[u], r = f_ptrs[u + 1] - 1;
+        for (int j = l; j <= r; j += 1)
+            visited[fwd_neighbors[j]] = 1;
+
+        for (int j = l; j <= r; j += 1)
+        {
+            v = fwd_neighbors[j];
+            L = f_ptrs[v], R = f_ptrs[v + 1] - 1;
+            for (int k = L; k <= R; k += 1)
+            {
+                w = fwd_neighbors[k];
+                if (visited[w])
+                    count += 1;
+            }
+        }
+
+        for (int j = l; j <= r; j += 1)
+            visited[fwd_neighbors[j]] = 0;
+    }
+
+    // for (auto v : values)
+    //     cout << v << " ";
+    // cout << "\n";
+    // for (auto ptr : ptrs)
+    //     cout << ptr << " ";
+    // cout << "\n";
+
+    cout << "The number of triangles present = " << count << ".\n";
+    return 0;
+}
